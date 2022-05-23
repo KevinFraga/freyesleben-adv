86c848162596f24af8cb69bb92c892ee17499d1b
KevinFragaCW <kevin.fraga@cloudwalk.io>
2022-05-23 02:04:42 -0300

    day-2: nostalgic

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,13 +5,10 @@ import "../styles/header.css"
 class Header extends Component {
     render() {
         return (
-            <div className="NavBar">                
-                <Link to="login">Login</Link>
-                <Link to="quem_somos">Quem Somos</Link>
-                <Link to="inspiracao">Inspiração</Link>
-                <Link to="especialidades">Especialidades</Link>
-                <Link to="cases_sucesso">Cases de Sucesso</Link>
-                <Link to="exclusivo_membros">Exclusivo para Membros</Link>
+            <div className="NavBar">
+                <svg width="60px" height="60px"><path d="M41,14H7a2,2,0,0,1,0-4H41A2,2,0,0,1,41,14Z" fill="#004aa2"/><path d="M41,26H7a2,2,0,0,1,0-4H41A2,2,0,0,1,41,26Z" fill="#004aa2"/><path d="M41,38H7a2,2,0,0,1,0-4H41A2,2,0,0,1,41,38Z" fill="#004aa2"/></svg>
+                <img src="logo.png" alt="logo" id="logo" />
+                <Link to='login'><img src="login-removebg-preview.png" alt="login" id="login" /></Link>
             </div>
         )
     }
