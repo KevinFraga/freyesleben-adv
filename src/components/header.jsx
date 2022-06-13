3b2cdbacfb1061bfd3db69734a47814160046e87
KevinFragaCW <kevin.fraga@cloudwalk.io>
2022-06-12 22:32:23 -0300

    header finished

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,19 +7,37 @@ class Header extends Component {
         super();
         this.state = {
             menu: false,
-            itens: ['Minha Conta', 'Quem Somos', 'Áreas de Atuação', 'Ações em Destaque', 'Nossos Parceiros', 'Depoimentos', 'Blog', 'Fale Conosco', 'Contato']
+            loggedIn: false,
+            featured: false
         };
         this.toggleMenu = this.toggleMenu.bind(this);
+        this.toggleFeatured = this.toggleFeatured.bind(this);
     }
 
     toggleMenu = () => this.setState({ menu: !this.state.menu });
 
+    toggleFeatured = () => this.setState({ featured: !this.state.featured });
+
     hamburguerMenu() {
-        const { itens, menu } = this.state;
+        const { loggedIn, menu, featured } = this.state;
         return (
             <div className={menu ? 'open' : 'close'}>
                 <ul className='menu-items'>
-                    {itens.map((item) => <li key={item}>{item}</li>)}
+                    <Link to={loggedIn ? 'conta' : 'login'}><li>Minha Conta</li></Link>
+                    <Link to='quemsomos'><li>Quem Somos</li></Link>
+                    <li onClick={this.toggleFeatured}>Ações em Destaque</li>
+                    <ul className={featured ? 'seen' : 'unseen'}>
+                        <Link to='causas/#fgts'><li>Revisão de Valores FGTS</li></Link>
+                        <Link to='causas/#inventAERUS'><li>Ação Inventário: Benefício AERUS</li></Link>
+                        <Link to='causas/#emprestAERUS'><li>Ação Monitória Empréstimos AERUS</li></Link>
+                        <Link to='causas/#rateio'><li>Recebimento de Rateios</li></Link>
+                    </ul>
+                    <Link to='parceiros'><li>Nossos Parceiros</li></Link>
+                    <Link to='depoimentos'><li>Depoimentos</li></Link>
+                    <Link to='faleconosco'><li>Fale Conosco</li></Link>
+                    <Link to='blog'><li>Blog</li></Link>
+                    <Link to='contato'><li>Contato</li></Link>
+                    <Link to='/'><li>Sair</li></Link>
                 </ul>
             </div>
         )
@@ -32,9 +50,9 @@ class Header extends Component {
                 <div className="NavBar">
                     <div className={menu ? 'open' : 'close'} onClick={this.toggleMenu}>
                         <div className="hamburger-lines">
-                            <span className="line line1"></span>
-                            <span className="line line2"></span>
-                            <span className="line line3"></span>
+                            <span className="line line1" />
+                            <span className="line line2" />
+                            <span className="line line3" />
                         </div>
                     </div>
                     <img src="logo.png" alt="logo" id="logo" />
