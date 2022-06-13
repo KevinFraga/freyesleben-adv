7cbaea98f946c373ce268f5c88ef2c7e1b1f0919
KevinFragaCW <kevin.fraga@cloudwalk.io>
2022-06-13 00:32:36 -0300

    hub construction start

---
+++ src/components/hub.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Hub extends Component {
+    constructor() {
+        super();
+        this.state = {
+            slide: 0
+        };
+    }
+
+    render() {
+        return (
+            <p>Teste</p>
+        );
+    }
+}
+
+export default Hub;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../components/header';
+import Hub from '../components/hub';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Hub />
       </div>
     );
   }
